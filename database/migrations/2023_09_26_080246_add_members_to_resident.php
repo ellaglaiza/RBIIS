259e1b11ace27b547a018d1ed4fc9da8e286e2b5
rosemie <llanarosemie10@gmail.com>
2023-11-23 17:33:58 +0800

    update the column

--- database/migrations/2023_09_26_080246_add_members_to_resident.php
+++ database/migrations/2023_09_26_080246_add_members_to_resident.php
@@ -21,27 +21,14 @@ return new class extends Migration
             $table->string('region')->after('province');
             $table->string('landlineNo')->after('region')->nullable();
             $table->string('contact_number')->after('landlineNo');
-            $table->string('email')->after('contact_number')->nullable();
-            $table->string('mother_name')->after('email');
-            $table->string('father_name')->after('mother_name');
-            $table->string('guardian')->after('father_name')->nullable();
-            $table->string('senior_HS')->after('guardian')->nullable();
-
-            $table->string('year_graduated1')->after('senior_HS')->nullable()->change();
-            $table->string('vocational')->after('year_graduated1')->nullable();
-            $table->year('year_graduated2')->after('vocational')->nullable();
-            $table->year('year_graduated3')->after('year_graduated2')->nullable();
-            $table->year('year_graduated4')->after('year_graduated3')->nullable();
-            $table->year('year_graduated5')->after('year_graduated4')->nullable();
-            $table->string('occupation')->after('year_graduated5')->nullable();
-
+            $table->string('email')->after('contact_number')->nullable();           
+            $table->string('occupation')->after('email')->nullable();
             $table->string('status_of_employment')->after('occupation')->nullable();
-
-            $table->string('type_of_employment')->after('status_of_employment')->nullable();
-            $table->string('category_of_employment')->after('type_of_employment')->nullable();
             $table->string('members_name')->after('category_of_employment')->nullable();
             $table->string('members_dob')->after('members_name')->nullable();
             $table->string('members_relationship')->after('members_dob')->nullable();
+            $table->string('householdNO')->after('members_relationship')->nullable();
+
 
 
         });
